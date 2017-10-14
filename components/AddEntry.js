d8bf259aa96da53368b4ac0270ed350bfb6fcc2a
Ugyen Jigten <ujigten@gmail.com>
2017-10-14 21:19:26 +0700

    feat: Add local notification

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue, clearLocalNotification, setLocalNotification } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
 import DateHeader from './DateHeader'
@@ -11,6 +11,7 @@ import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { white, purple } from '../utils/colors'
 import { NavigationActions } from 'react-navigation'
+
 
 function SubmitBtn ({onPress}) {
   return (
@@ -64,7 +65,8 @@ class AddEntry extends Component {
 
     submitEntry({ key, entry })
 
-    // Clear local notification
+    clearLocalNotification()
+      .then(setLocalNotification)
   }
 
   decrement = (metric) => {
